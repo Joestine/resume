78c11690ea4bcec9a0e0efd031c1b56150694485
Justin <justinvdlaang@gmail.com>
2024-03-31 14:16:12 +0200

    mobile update

--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -4,8 +4,8 @@ import { NavbarItem } from "./navbaritem";
 
 export function Navbar() {
 	return (
-		<nav className="max-w-sm md:max-w-3xl fixed top-[-20px] md:top-auto md:bottom-[-20px] left-0 right-0 mx-auto">
-			<BackgroundGradient className="flex gap-10 justify-center items-center rounded-[22px] h-24 max-w-md md:max-w-4xl p-2  bg-zinc-900">
+		<nav className="max-w-s w-fit md:w-auto md:max-w-3xl fixed top-2 md:top-auto md:bottom-[-20px] left-0 right-0 mx-auto">
+			<BackgroundGradient className="flex gap-6 md:gap-10 justify-center items-center rounded-[22px] h-16 md:h-24 max-w-md md:max-w-4xl p-2  bg-zinc-900">
 				<NavbarItem href="/" text="Home"></NavbarItem>
 				<NavbarItem href="./about" text="About Me"></NavbarItem>
 				<NavbarItem href="./experience" text="Experience"></NavbarItem>
